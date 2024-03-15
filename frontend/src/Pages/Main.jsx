5f9233a354880f20d46a54e58e5103f0e0b1bdb6
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2024-03-15 20:40:25 +0200

    extra trust

--- frontend/src/Pages/Main.jsx
+++ frontend/src/Pages/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Style.css';
 import './Animations.css';
+import board from '../Assets/BoardReal.png';
 
 function Main() {
     return <div className="relative h-screen bg-gray-100">
     <div className="flying-dart dart1"></div>
     <div className="flying-dart dart2"></div>
     <div className="flying-dart dart3"></div>
+    <div className="Board">
+      <img src={board} alt="Sample" />
+    </div>
   </div>
   }
   
