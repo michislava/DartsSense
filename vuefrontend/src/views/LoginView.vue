c9a191e3d13b66ab7fd1ba2184ec7c0d2f502eea
michislava <georgievaemillia@gmail.com>
2024-03-30 23:57:24 +0200

    dai Boje

---
+++ vuefrontend/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import LoggingIn from '../components/LoggingIn.vue';
+</script>
+
+<template>
+  <main>
+    <LoggingIn/>
+  </main>
+</template>
+
+<script>
+export default {
+  components: {
+    LoggingIn,
+  }
+}
+</script>
