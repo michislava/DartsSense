3cc9d5cd9d086ae425884f7a8dbaa8fe0fd990dc
i-kratko <ikratkodev@gmail.com>
2024-04-04 15:57:08 +0300

    Adjusted some home page animations

--- vuefrontend/src/components/Animation.vue
+++ vuefrontend/src/components/Animation.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="greetings">
+  <div>
 
-    <div className="fadeText">
+    <div class="fadeText" :class="{ 'visible': isVisible }">
       <h1>DartsSense</h1>
     </div>
 
@@ -9,9 +9,9 @@
       <img src="../assets/images/BoardReal.png" alt="Sample" />
     </div>
 
-    <Dart :startX=1000 :startY=-100 :endX=-600 :endY=500 />
-    <Dart :startX=1000 :startY=-100 :endX=-550 :endY=220 />
-    <Dart :startX=1000 :startY=-100 :endX=-450 :endY=110 />
+    <Dart :startX=1980 :startY=-100 :endX=-530 :endY=500 />
+    <Dart :startX=1980 :startY=-100 :endX=-470 :endY=220 />
+    <Dart :startX=1980 :startY=-100 :endX=-370 :endY=110 />
   </div>
 </template>
 
@@ -22,21 +22,26 @@ export default {
     components: {
         Dart
     },
-    
+    data() {
+     return {
+        isVisible: false
+      };
+    },
+    mounted() {
+    // Trigger the fade-in animation after a delay to allow the component to render
+    setTimeout(() => {
+      this.isVisible = true;
+      console.log("benson");
+    }, 3000); // 3 seconds delay
+  }
 }
 
 </script>
 
 <style scoped>
-  
-  .flying-dart1 {
-    position: absolute;
-    width: 60px; /* Adjust width and height as needed */
-    height: 40px;
-    background-image: url('../assets/images/dart-illustration.png'); /* Use custom image */
-    background-size: cover;
-    animation: fly 2s linear forwards;
-    animation-delay: 0s;
+  @keyframes fadeIn {
+    0% { opacity: 0; }
+    100% { opacity: 1; }
   }
 
   .Board {
@@ -45,17 +50,24 @@ export default {
     position: relative;
     z-index: -1;
     top: 140px; /* Adjust top position as needed */
-    left: -800px;
+    left: -700px;
+  }
+
+  .visible {
+    opacity: 1;
   }
 
   .fadeText {
-    font-size: 50px;
-    justify-content: center;
-    color: #FFD700;
-    animation: fadeIn 2s;
+    opacity: 0;
+
+    font-size: 80px;
+    justify-content: absolute;
+    font-weight: 1;
+    color: #f3d99b;
+    animation: fadeIn 1.5s ease forwards;
+    animation-delay: 4.5s;
     position: relative;
     margin-top: 30px;
-    left: -50px; /* Move the h1 50 pixels to the right */
-    top: 333px; /* Move the h1 20 pixels down */
+    top: 50px;
 }
 </style>
