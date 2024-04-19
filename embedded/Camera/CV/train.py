75c648e955ba6b08187e3d60f2eb67da181b6124
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2024-04-19 08:59:15 +0300

    confusionmaxxing

--- embedded/Camera/CV/train.py
+++ embedded/Camera/CV/train.py
@@ -1,14 +1,6 @@
 from ultralytics import YOLO
 import torch
 
-model = YOLO("yolov8s.yaml")
-
-<<<<<<< HEAD
-#print(torch.cuda.is_available())
-
 model = YOLO("yolov8n.yaml")
 
-results = model.train(data=r"C:\Users\victo\Desktop\Github REPOS\DartsSense\embedded\Camera\CV\config.yaml", epochs=1, device=0)
-=======
-results = model.train(data=r"C:\Users\Viktorio\Desktop\DartsSense\DartsSense\embedded\Camera\CV\config.yaml", epochs=20)
->>>>>>> 6c397215b2d58f2df22b73d32654fe5eef96f0d1
+results = model.train(data=r"C:\Users\Viktorio\Desktop\DartsSense\DartsSense\embedded\Camera\CV\config.yaml", epochs=40)
